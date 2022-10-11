c05ace90509b14162863bfa080406a67686b24fb
Angela Busato <busato.angela@gmail.com>
2022-10-11 16:10:04 +0200

    exercies' solutions

--- src/02-object-param.problem.ts
+++ src/02-object-param.problem.ts
@@ -1,6 +1,11 @@
 import { expect, it } from "vitest";
 
-export const addTwoNumbers = (params) => {
+interface TwoNumbers {
+  first: number, 
+  second: number
+}
+
+export const addTwoNumbers = (params: TwoNumbers) => {
   return params.first + params.second;
 };
 
